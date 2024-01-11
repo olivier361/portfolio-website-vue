f7632fe514e9922af4c1d585cfe039fcdc267329
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2024-01-11 13:10:46 -0800

    PWV-34 - WIP NavHeader component

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="header">
+    <img alt="Site logo" src="@/assets/ogg-logo-long.png" width="auto" height="26" />
+    <div class="nav-links">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">Resume</RouterLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: white
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+.header {
+  width: 100%;
+  height: 50px;
+  background-color: #232323;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.nav-links {
+  width: auto;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+</style>
