0c109e77273b873994cf4e8c535a34691f799f61
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2024-01-11 14:12:26 -0800

    PWV-34 - Ad basic header styling

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -4,22 +4,33 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <div class="header">
-    <img alt="Site logo" src="@/assets/ogg-logo-long.png" width="auto" height="26" />
+    <RouterLink to="/" style="height: 26px">
+      <img alt="Site logo" src="@/assets/ogg-logo-long.png" width="auto" height="26" />
+    </RouterLink>
     <div class="nav-links">
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">Resume</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
     </div>
   </div>
 </template>
 
 <style scoped>
-a {
-  color: white
+/* a {
+  color: white;
+  text-decoration: none;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  border-bottom: 2px solid transparent;
+  transition: color 200ms, border-bottom 200ms;
 }
 
 a:hover {
-  text-decoration: underline;
-}
+  color: #FFD700;
+  /* text-decoration: underline;
+  border-bottom: 2px solid #FFD700;
+} */
 
 .header {
   width: 100%;
@@ -28,6 +39,7 @@ a:hover {
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding: 0px 20px;
 }
 
 .nav-links {
@@ -37,4 +49,20 @@ a:hover {
   justify-content: flex-end;
 }
 
+.nav-links > * {
+  margin-left: 20px;
+  color: white;
+  text-decoration: none;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  border-bottom: 2px solid transparent;
+  transition: color 200ms, border-bottom 200ms;
+}
+
+.nav-links > *:hover {
+  color: #FFD700;
+  /* text-decoration: underline; */
+  border-bottom: 2px solid #FFD700;
+}
 </style>
