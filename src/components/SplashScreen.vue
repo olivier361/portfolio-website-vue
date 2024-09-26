8c86e9385d43113eff7fff9f60159e7ceb04c754
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2024-09-25 20:52:49 -0400

    PWV-38 - Add basic dotnav  to SplashScreen

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -24,13 +24,7 @@
       <a class="uk-position-center-left uk-position-small uk-hidden-hover" href uk-slidenav-previous uk-slideshow-item="previous"></a>
       <a class="uk-position-center-right uk-position-small uk-hidden-hover" href uk-slidenav-next uk-slideshow-item="next"></a>
     
-      <!-- <ul>
-        <li uk-slideshow-item="0">0</li>
-        <li uk-slideshow-item="1">1</li>
-        <li uk-slideshow-item="2">2</li>
-      </ul> -->
-
-      <!-- <ul class="uk-slideshow-nav uk-dotnav uk-position-bottom-center uk-position-small"></ul> -->
+      <ul class="uk-slideshow-nav uk-dotnav uk-position-bottom-center uk-position-small"></ul>
     </div>
 
   </div>
