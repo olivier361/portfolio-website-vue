a8d90d27b85c6f6c7af32d475a68d3c6c2dd350d
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2024-09-27 15:47:25 -0400

    PWV-38 - SplashScreen Slideshow improvements

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -5,24 +5,24 @@
 <template>
 
   <div class="splashscreen">
-    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: pull; autoplay: true">
+    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: pull; autoplay: true; pause-on-hover: false">
       <div class="uk-slideshow-items">
         <div>
-          <img src="@/assets/2D-Pixel-Game.png" alt="" uk-cover>
+          <img src="@/assets/2D-Pixel-Game.png" alt="2D Pixel Platformer Game" uk-cover>
         </div>
         <div>
-          <img src="@/assets/cube-game.png" alt="" uk-cover>
+          <img src="@/assets/cube-game.png" alt="Cube Game" uk-cover>
         </div>
         <div>
-          <img src="@/assets/ruby-adventure.png" alt="" uk-cover>
+          <img src="@/assets/ruby-adventure.png" alt="Ruby's Adventure Game" uk-cover>
         </div>
       </div>
 
       <!-- TODO: Added as a test -->
       <h1 class="uk-position-center" style="margin: 0;">Welcome to the Splash Screen</h1>
 
-      <a class="uk-position-center-left uk-position-small uk-hidden-hover" href uk-slidenav-previous uk-slideshow-item="previous"></a>
-      <a class="uk-position-center-right uk-position-small uk-hidden-hover" href uk-slidenav-next uk-slideshow-item="next"></a>
+      <a class="uk-position-center-left uk-position-small uk-hidden-hover" uk-slidenav-previous uk-slideshow-item="previous"></a>
+      <a class="uk-position-center-right uk-position-small uk-hidden-hover" uk-slidenav-next uk-slideshow-item="next"></a>
     
       <ul class="uk-slideshow-nav uk-dotnav uk-position-bottom-center uk-position-small"></ul>
     </div>
