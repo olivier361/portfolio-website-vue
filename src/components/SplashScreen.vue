15a4ceea71e590bf019fa262c51db12a24ef3c06
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2024-10-01 17:25:49 -0400

    PWV-38 - WIP various attempts dynamic heading style

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -1,5 +1,7 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+const windowWidth = ref(window.innerWidth);
 
 const items = ref([
   'Software & Game Developer',
@@ -7,28 +9,74 @@ const items = ref([
   'Design and Marketing',
 ]);
 
+const versions = ref([
+  'Software & Game Developer • Video Content Creator • Design and Marketing',
+  'Software & Game Developer • Video Content Creator<br>Design and Marketing',
+  'Software & Game Developer<br>• Video Content Creator •<br>Design and Marketing',
+]);
+
+const handleResize = () => {
+  windowWidth.value = window.innerWidth;
+};
+
 const flexContainer = ref(null);
-const itemPositions = ref([]);
+// const itemPositions = ref([]);
 
-const updateItemPositions = () => {
-  const children = flexContainer.value.children;
-  itemPositions.value = Array.from(children).map((item) =>
-    item.getBoundingClientRect().top
-  );
-};
+// Utility function: debounce to delay function execution
+// function debounce(fn, delay) {
+//   let timeout;
+//   return function (...args) {
+//     clearTimeout(timeout);
+//     timeout = setTimeout(() => {fn.apply(this, args); fn.apply(this, args)}, delay);
+//   };
+// }
 
-const shouldAddBullet = (index) => {
-  // Add bullet if the current item and the next are on the same line
-  console.log(`itemPositions.value[index]: ${itemPositions.value}`);
-  return (
-    index < itemPositions.value.length - 1 &&
-    itemPositions.value[index] === itemPositions.value[index + 1]
-  );
-};
+// const updateItemPositions = () => {
+//   const children = flexContainer.value.children;
+//   itemPositions.value = Array.from(children).map((item) =>
+//     item.getBoundingClientRect().top
+//   );
+// };
+
+// Debounced version of the updateItemPositions function
+// const debouncedUpdate = debounce(updateItemPositions, 500); // 100ms delay
+
+// const shouldAddBullet = (index) => {
+//   // Add bullet if the current item and the next are on the same line
+//   // console.log(`itemPositions.value[index]: ${itemPositions.value}`);
+//   return (
+//     index < itemPositions.value.length - 1 &&
+//     itemPositions.value[index] === itemPositions.value[index + 1]
+//   );
+// };
+
+// function updateBullets() {
+//   // if flexContainer width is less than 640px, set display none to bullet-2 class items
+//   if (flexContainer.value.offsetWidth < 960) {
+//     document.querySelectorAll('.bullet-2').forEach((bullet) => {
+//       bullet.style.display = 'none';
+//     });
+//   } else {
+//     document.querySelectorAll('.bullet-2').forEach((bullet) => {
+//       bullet.style.display = 'initial';
+//     });
+//   }
+  
+// }
 
 onMounted(() => {
-  updateItemPositions();
-  window.addEventListener('resize', updateItemPositions);
+  // updateItemPositions();
+  // window.addEventListener('resize', debouncedUpdate);
+  // updateBullets();
+  // window.addEventListener('resize', updateBullets);
+  window.addEventListener('resize', handleResize);
+
+});
+
+onBeforeUnmount(() => {
+//   window.removeEventListener('resize', debouncedUpdate);
+window.removeEventListener('resize', handleResize);
+
 });
 
 </script>
@@ -61,7 +109,41 @@ onMounted(() => {
           <img id="main-logo" src="@/assets/ogg-logo-tall.png" alt="Olivier Gervais-Gougeon" width="100%">
         </div>
 
-        <h3 class="flex-container uk-position-center" ref="flexContainer">
+        <!-- <h3 class="uk-position-center uk-visible@l uk-hidden" ref="flexContainer">
+          {{ versions[0] }}
+        </h3>
+        <h3 class="uk-position-center uk-hidden@m uk-visible@m" ref="flexContainer">
+          {{ versions[1] }}
+        </h3>
+        <h3 class="uk-position-center uk-hidden@s" ref="flexContainer">
+          {{ versions[2] }}
+        </h3> -->
+
+        <!-- if window width between 960 and 640 show versions[0] else if between 639 and 420 show versions[1] else show versions[2] -->
+        <h3 class="uk-position-center" v-if="windowWidth > 960">
+          Software & Game Developer • Video Content Creator • Design and Marketing
+        </h3>
+        <h3 class="uk-position-center" v-else-if="640 < windowWidth && windowWidth <= 960">
+          Software & Game Developer • Video Content Creator<br>Design and Marketing
+        </h3>
+        <h3 class="uk-position-center" style="line-height: 0.8;" v-else>
+          Software & Game Developer<br>—<br>Video Content Creator<br>—<br>Design and Marketing
+        </h3>
+
+
+
+        <!-- <h3 class="uk-position-center" ref="flexContainer">
+          <span class="line">{{ items[0] }}</span>
+          <span class="line bullet-1" v-if="flexContainer?.value && flexContainer?.value.offsetWidth < 960">|<br></span>
+          <span class="line bullet-1" v-else>&nbsp;•&nbsp;</span>
+          <span class="line bullet-1">&nbsp;•&nbsp;</span>
+          <span class="line">{{ items[1] }}</span>
+          <span class="line bullet-2" v-if="flexContainer?.value && flexContainer?.value.offsetWidth < 960">|<br></span>
+          <span class="line bullet-2" v-else>&nbsp;•&nbsp;</span>
+          <span class="line bullet-2">&nbsp;•&nbsp;</span>
+          <span class="line">{{ items[2] }}</span>
+        </h3> -->
+        <!-- <h3 class="flex-container uk-position-center" ref="flexContainer">
           <span class="flex-item"
             v-for="(item, index) in items"
             :key="index"
@@ -69,7 +151,7 @@ onMounted(() => {
           >
             {{ item }}
           </span>
-        </h3>
+        </h3> -->
       </div>
       
       <a class="uk-position-center-left uk-position-small uk-hidden-hover" uk-slidenav-previous uk-slideshow-item="previous"></a>
@@ -105,19 +187,21 @@ onMounted(() => {
     position: absolute;
     top: 22vh;
     margin: 0px;
+    width: 100%;
     color: var(--color-splashscreen-text);
     text-shadow: 2px 2px 10px rgba(0,0,0,0.70); /* Horizontal, Vertical, Blur, Color */
-/* 
+
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
 
+    text-align: center;
     span.line {
       white-space: nowrap;
       display: inline-block;
-    } */
+    }
   }
 
   .social-links {
@@ -200,13 +284,15 @@ onMounted(() => {
 }
 
 /* Add bullet after the element if the class 'bullet' is applied */
-.bullet::after {
+/* .bullet::after {
   content: "•";
-  padding: 0px 10px;
+  width: 40px;
+  /* padding: 0px 10px; commented out
   color: var(--color-splashscreen-text);
-}
+} */
 
-/* .flex-item:last-child::after {
+/*
+.flex-item:last-child::after {
   content: "";
 } */
 
