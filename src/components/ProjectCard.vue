cb83c611114a88aad7c9b743db3693604f730fee
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2024-10-24 18:22:54 -0700

    PWV-55 - WIP expand card smooth transition

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -26,7 +26,7 @@ function handleCardExpand(){
 
 <template>
 
-  <div class="project-card">
+  <div :class="isExpanded ? 'project-card expanded' : 'project-card'">
     <div class="preview-section">
       <h2>{{ heading }}</h2>
       <p>{{ introParagraph }}</p>
@@ -35,9 +35,12 @@ function handleCardExpand(){
       </div>
     </div>
     <hr class="preview-divider" v-if="isExpanded"/>
-    <div class="info-section" v-if="isExpanded">
+    <!-- TODO: WIP not sure if this is the good approach -->
+    <transition name="fade">
+      <div class="info-section" v-if="isExpanded">
       <slot>This is where the content coming from the parent should go.</slot>
-    </div>
+      </div>
+    </transition>
   </div>
 
 </template>
@@ -50,6 +53,9 @@ function handleCardExpand(){
   background-color: var(--color-card-background);
   border-radius: var(--card-border-radius);
   /* transition: all 0.3s; */
+  max-height: 200px;
+  transition: max-height 3.0s ease;
+  overflow: hidden;
 
   .preview-section {
     /* -5px in the margin calculation is to account for .expand-button padding to have 25px total spacing */
@@ -80,6 +86,37 @@ function handleCardExpand(){
     border-top: 5px solid var(--color-card-divider);
     margin: 0px;
   }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+
+/* .slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+} */
+
+
+.project-card.expanded {
+  max-height: 2000px;
+  transition: max-height 3.0s ease;
 }
 
 </style>
