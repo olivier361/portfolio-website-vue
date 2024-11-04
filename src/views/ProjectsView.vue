19dc13c338dc9d727ce2eadc74b8e171fa23532d
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2024-11-04 14:52:48 -0800

    PWV-56 - add more content to sample ProjectCard

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import ProjectCard from '@/components/ProjectCard.vue';
 import ImageCollection from '@/components/ImageCollection.vue';
+import ImageFrame from '@/components/ImageFrame.vue';
 </script>
 
 <template>
@@ -21,9 +22,33 @@ import ImageCollection from '@/components/ImageCollection.vue';
           <br><br>
           A few more lines of text for these cards to be a bit longer. These cards will have a "view all" button to open and close the details of the card below the thich white line.
         </template>
-        Test content from parent!
+
+        <h3>Project Title 1</h3>
+        <div class="uk-flex" :style="{columnGap: '50px', marginBottom: '50px'}">
+          <p class="uk-flex-1">
+            Three to twelve sentences describing the project in general. These sentences can be split into multiple paragraphs if needed.
+            Projects that require lengthy detailed explanations will actually only feature a brief summary of the key "must know" details
+            on the project page. They will include a button at the end of their project section. This button will direct users to a full
+            dedicated page talking about the project.
+            <br><br>
+            Projects that can be summarized more concisely may opt to not have a dedicated page and instead explain the entirety of what
+            needs to be said right here in the general project page, given that it fits in the 3 to 12 sentence approximate requirement.
+            <br><br>
+            <b>
+              Projects with many photos like this one will likely not have a read more button since everything is likely well explained
+              and showcased here.
+            </b>
+            <br><br>
+            Notice how we used bold sentences mid-paragraph above.
+          </p>
+          <ImageFrame
+            class="uk-flex-1"
+            imgPath="2D-Pixel-Game.png"
+            captionText="Here is an image caption. its italic and smaller than regular text. Ideally a different font even maybe and shouldn't be longer than two to three lines of text."
+            altText="A screenshot of a colorful 16-bit 2D platformer featuring a fox character."
+          />
+        </div>
         <ImageCollection :imgList="[
-            { imgPath: '2D-Pixel-Game.png', captionText: '2D Pixel Platformer Game. Making the text longer to test how captions wrap when they are longer than the displayed image above.', altText: 'A screenshot of a colorful 16-bit 2D platformer featuring a fox character.' },
             { imgPath: 'cube-game.png', captionText: 'Cube Game. Making the text longer to test how captions wrap when they are longer than the displayed image above. Let\'s make this row even longer to test image spacing when a caption is longer than others.', altText: 'An endless runner game featuring a red cube character on a green surface with obstacles ahead.' },
             { imgPath: 'ruby-adventure.png', captionText: 'Ruby\'s Adventure Game. Making the text longer to test how captions wrap when they are longer than the displayed image above.', altText: 'A top-down 2D game featuring multiple fantasy characters walking in a prototype level.' },
             { imgPath: 'test-image.png', captionText: 'A test image with a long caption to test how the caption wraps when it is longer than the image. The caption needs to be very long to wrap around even a big photo.', altText: 'test image' }
@@ -31,6 +56,62 @@ import ImageCollection from '@/components/ImageCollection.vue';
           imgWidth="320px"
           imgHeight="180px"
         />
+
+        <hr class="info-divider"/>
+
+        <h3>Project Title 2</h3>
+        <div class="uk-flex" :style="{columnGap: '50px'}">
+          <p class="uk-flex-1">
+            Three to twelve sentences describing the project in general. These sentences can be split into multiple paragraphs if needed.
+            Projects that require lengthy detailed explanations will actually only feature a brief summary of the key "must know" details
+            on the project page. They will include a button at the end of their project section. This button will direct users to a full
+            dedicated page talking about the project.
+            <br><br>
+            Projects that can be summarized more concisely may opt to not have a dedicated page and instead explain the entirety of what
+            needs to be said right here in the general project page, given that it fits in the 3 to 12 sentence approximate requirement.
+            <br><br>
+            <b>
+              Projects with many photos like this one will likely not have a read more button since everything is likely well explained
+              and showcased here.
+            </b>
+            <br><br>
+            Notice how we used bold sentences mid-paragraph above.
+          </p>
+          <ImageFrame
+            class="uk-flex-1"
+            imgPath="ruby-adventure.png"
+            captionText="Here is an image caption. its italic and smaller than regular text. Ideally a different font even maybe and shouldn't be longer than two to three lines of text."
+            altText="A screenshot of a colorful 16-bit 2D platformer featuring a fox character."
+          />
+        </div>
+
+        <hr class="info-divider"/>
+
+        <h3>Project Title 3</h3>
+        <div class="uk-flex" :style="{columnGap: '50px'}">
+          <p class="uk-flex-1">
+            Three to twelve sentences describing the project in general. These sentences can be split into multiple paragraphs if needed.
+            Projects that require lengthy detailed explanations will actually only feature a brief summary of the key "must know" details
+            on the project page. They will include a button at the end of their project section. This button will direct users to a full
+            dedicated page talking about the project.
+            <br><br>
+            Projects that can be summarized more concisely may opt to not have a dedicated page and instead explain the entirety of what
+            needs to be said right here in the general project page, given that it fits in the 3 to 12 sentence approximate requirement.
+            <br><br>
+            <b>
+              Projects with many photos like this one will likely not have a read more button since everything is likely well explained
+              and showcased here.
+            </b>
+            <br><br>
+            Notice how we used bold sentences mid-paragraph above.
+          </p>
+          <ImageFrame
+            class="uk-flex-1"
+            imgPath="cube-game.png"
+            captionText="Here is an image caption. its italic and smaller than regular text. Ideally a different font even maybe and shouldn't be longer than two to three lines of text."
+            altText="A screenshot of a colorful 16-bit 2D platformer featuring a fox character."
+          />
+        </div>
       </ProjectCard>
       <ProjectCard 
         :heading="'Project 2'"
@@ -65,6 +146,12 @@ import ImageCollection from '@/components/ImageCollection.vue';
 
 .project-card {
   margin: 20px 0px;
+}
+
+hr.info-divider {
+  width: 100%;
+  border-top: 1px solid var(--color-card-info-divider);
+  margin: 50px 0px;
 }
 
 /* @media (min-width: 1024px) {
