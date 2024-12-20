0aadd8e7266317d1bf0eb0f98d8bc26df18a8f8b
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2024-12-19 17:04:45 -0800

    PWV-39 - Setup file structure for test suite

--- vitest.config.js
+++ vitest.config.js
@@ -2,11 +2,17 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
+// IMPORTANT: since we have a separate config file for Vite and Vitest,
+// make sure to define the same Vite options in your Vitest config file
+// since it will override your Vite file, not extend it.
+// Source: https://vitest.dev/guide/#configuring-vitest
+
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
       environment: 'jsdom',
+      // include: [...configDefaults.include],
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url))
     }
