a844760b5e19943cd4192b4b99568c6642a3aee1
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2025-01-15 22:11:19 -0800

    PWV-64 - Add all wanted ESLint problems and suggestions rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,8 +9,67 @@ module.exports = {
     ecmaVersion: 'latest'
   },
   'rules': {
+    // ESLint Rule Docs: https://eslint.org/docs/latest/rules/
+    /// ERRORS:
     // 'no-undef': 'warn' // default: 'error'
-    'block-scoped-var': 'error'
+    'block-scoped-var': 'error',
+    'no-duplicate-imports': 'error',
+    'no-template-curly-in-string': 'error',
+    'require-atomic-updates': 'error',
+    'default-case': 'error',
+    'default-case-last': 'error',
+    'eqeqeq': ['error', 'smart'],
+    'func-style': ['error', 'declaration', {
+      'allowArrowFunctions': true
+    }],
+    'max-classes-per-file': 'error',
+    'no-use-before-define': ['error', {
+        'functions': false,
+        'classes': false,
+        'variables': true,
+        'allowNamedExports': false
+    }],
+    'new-cap': ['error', {
+      'newIsCap': true
+    }],
+    'no-alert': 'error',
+    'no-empty-function': 'error',
+    'no-eval': 'error',
+    'no-implicit-globals': 'error',
+    'no-implied-eval': 'error',
+    'no-label-var': 'error',
+    'no-lone-blocks': 'error',
+    'no-lonely-if': 'error',
+    'no-loop-func': 'error',
+    'no-multi-assign': 'error',
+    'no-param-reassign': 'error',
+    'no-return-assign': 'error',
+    'no-script-url': 'error',
+    'no-shadow': 'error',
+    'no-unneeded-ternary': ['error', {
+      'defaultAssignment': false
+    }],
+    'no-unused-expressions': ['error', {
+      'allowTernary': true
+    }],
+    'no-useless-concat': 'error',
+    'no-useless-escape': 'error',
+    'no-var': 'error',
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'prefer-exponentiation-operator': 'error',
+    'prefer-promise-reject-errors': 'error',
+    // 'prefer-template': 'error',
+    'require-await': 'error',
+    // 'sort-imports': 'error',
+    /// WARNINGS:
+    'camelcase': 'warn',
+    'curly': ['warn', 'multi-line', 'consistent'],
+    'no-console': 'warn',
+    // 'no-warning-comments': ['warn', {
+    //   'terms': ['todo'],
+    //   'decoration': ['/', '*']
+    // }],
   },
   // 'overrides': [
   //   {
