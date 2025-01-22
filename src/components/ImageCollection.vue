6c9cf44ad1fa0cb301de7849d77ed1267522e0eb
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2025-01-21 16:43:19 -0800

    PWV-64 - Fix ESLint @stylistic/implicit-arrow-linebreak errors

--- src/components/ImageCollection.vue
+++ src/components/ImageCollection.vue
@@ -10,12 +10,13 @@ const props = defineProps({
     type: Array,
     required: true,
     validator(value) {
-      return value.every((item) =>
-        typeof item === 'object'
+      return value.every((item) => {
+        return typeof item === 'object'
         && (Object.hasOwn(item, 'imgPath') && typeof item.imgPath === 'string' && item.imgPath.length > 0)
         && (!Object.hasOwn(item, 'captionText') || (Object.hasOwn(item, 'captionText') && typeof item.captionText === 'string')) // captionText is optional but must be a string if provided
         && (!Object.hasOwn(item, 'altText') || (Object.hasOwn(item, 'altText') && typeof item.altText === 'string')) // altText is optional but must be a string if provided
-        && (!Object.hasOwn(item, 'isUrlPath') || (Object.hasOwn(item, 'isUrlPath') && typeof item.isUrlPath === 'boolean'))); // isUrl is optional but must be a boolean if provided
+        && (!Object.hasOwn(item, 'isUrlPath') || (Object.hasOwn(item, 'isUrlPath') && typeof item.isUrlPath === 'boolean')) // isUrl is optional but must be a boolean if provided
+      });
     },
   },
   imgHeight: {
