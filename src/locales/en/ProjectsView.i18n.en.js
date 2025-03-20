b6e598f3bca5dbfbacbbd7147ec5961ccaaaa8d9
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2025-03-20 16:28:02 -0700

    PWV-86 - Remove sample ProjectCards

--- src/locales/en/ProjectsView.i18n.en.js
+++ src/locales/en/ProjectsView.i18n.en.js
@@ -10,19 +10,6 @@ export default {
       videoEditing: 'Video Editing & Production',
       graphicDesign: 'Graphic Design',
     },
-    project1: {
-      title: 'Project 1',
-      introParaPart1: `
-        One or two sentence paragraphs describing the section in general.
-        EX: I've been making games in Unity since 2020.
-        Below are a handful of game prototypes I have made.
-      `,
-      introParaPart2: `
-        A few more lines of text for these cards to be a bit longer.
-        These cards will have a "view all" button to open and close
-        the details of the card below the thich white line.
-      `,
-    },
     unity: {
       title: 'Unity Games',
       introPara1: `
