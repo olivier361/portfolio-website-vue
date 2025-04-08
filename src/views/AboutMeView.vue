0eba7f91dce92ad8f1d9ea721dd7b95d3c03a979
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2025-04-07 19:21:52 -0700

    PWV-102 - Add english localization text for About Me page

--- src/views/AboutMeView.vue
+++ src/views/AboutMeView.vue
@@ -1,7 +1,26 @@
+<script setup>
+import { useI18n } from 'vue-i18n';
+import en from '@/locales/en/AboutMeView.i18n.en.js';
+
+import CTAButton from '@/components/CTAButton.vue';
+import ImageFrame from '@/components/ImageFrame.vue';
+import SectionButton from '@/components/SectionButton.vue';
+
+const { t } = useI18n({
+  messages: { en: en.en },
+});
+
+</script>
+
 <template>
-  <div class="about-me">
-    <h1>This is an about me page</h1>
-  </div>
+  <main>
+    <div class="about-me">
+      <h1>{{ t('title') }}</h1>
+      <div class="content-card">
+        <!-- TODO: Add content here -->
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
