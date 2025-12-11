e8a4956b6a65766f3e0901eaa0de6da93053152c
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2025-12-10 16:50:09 -0800

    PWV-117 - Add getCurrentYear() helper unit tests

--- tests/unit/helpers.test.js
+++ tests/unit/helpers.test.js
@@ -1,6 +1,6 @@
 import { suite, test, expect, vi } from 'vitest';
 
-import { yearsSinceString, getAssetsSiteUrl } from '@/utils/helpers.js';
+import { yearsSinceString, getCurrentYear, getAssetsSiteUrl } from '@/utils/helpers.js';
 
 suite('yearsSinceString() - Unit Tests', () => {
 
@@ -194,6 +194,147 @@ suite('yearsSinceString() - Unit Tests', () => {
       // restore the real system time for other tests
       vi.useRealTimers();
     });
+
+});
+
+suite('getCurrentYear() - Unit Tests', () => {
+
+  test('getCurrentYear() - returns current year correctly - general case', () => {
+    // create mocks for console.error and console.warn to track its calls
+    // and prevent it from actually logging in the test suite
+    const spyError = vi.spyOn(console, 'error').mockImplementation(() => { return; });
+    const spyWarn = vi.spyOn(console, 'warn').mockImplementation(() => { return; });
+
+    const currentYear = 2007;
+    const fallback = 2000;
+
+    // mock the system date for testing
+    const mockDate = new Date(currentYear, 5, 29); // June 29, 2007
+
+    vi.useFakeTimers();
+    vi.setSystemTime(mockDate);
+
+    const result = getCurrentYear(fallback);
+
+    expect(result).toBe(currentYear);
+
+    // no errors logged on happy path
+    expect(spyError).not.toHaveBeenCalled();
+    expect(spyWarn).not.toHaveBeenCalled();
+
+    // restore the real system time for other tests
+    vi.useRealTimers();
+  });
+
+  test.each([
+    [2023, 2024, true], // uses fallback
+    [1950, 1949, false], // uses computed year
+    [2010, 2010, false], // uses computed year
+  ])('getCurrentYear() - returns fallback if computed current year is smaller - cur=%d, fal=%d',
+    (currentYear, fallback, returnsFallback) => {
+      // create mocks for console.error and console.warn to track its calls
+      // and prevent it from actually logging in the test suite
+      const spyError = vi.spyOn(console, 'error').mockImplementation(() => { return; });
+      const spyWarn = vi.spyOn(console, 'warn').mockImplementation(() => { return; });
+
+      // mock the system date for testing
+      const mockDate = new Date(currentYear, 0, 1); // January 1, currentYear
+
+      vi.useFakeTimers();
+      vi.setSystemTime(mockDate);
+
+      const result = getCurrentYear(fallback);
+
+      expect(result).toBe(returnsFallback ? fallback : currentYear);
+
+      // no errors logged on happy path
+      expect(spyError).not.toHaveBeenCalled();
+      expect(spyWarn).not.toHaveBeenCalled();
+
+      // restore the real system time for other tests
+      vi.useRealTimers();
+    });
+
+  test('getCurrentYear() - returns undefined and logs error when fallback is not a number', () => {
+    // create mocks for console.error and console.warn to track its calls
+    // and prevent it from actually logging in the test suite
+    const spyError = vi.spyOn(console, 'error').mockImplementation(() => { return; });
+    const spyWarn = vi.spyOn(console, 'warn').mockImplementation(() => { return; });
+
+    const currentYear = 2007;
+    const fallback1 = '2000'; // invalid fallback. String is not a number.
+    const fallback2 = 2000.5; // invalid fallback. Float is not an integer.
+
+    // mock the system date for testing
+    const mockDate = new Date(currentYear, 5, 29); // June 29, 2007
+
+    vi.useFakeTimers();
+    vi.setSystemTime(mockDate);
+
+    // TEST 1: fallback is a string
+    const result1 = getCurrentYear(fallback1);
+
+    expect(result1).toBeUndefined();
+
+    // log error when fallback is not a number
+    expect(spyError).toHaveBeenCalledOnce();
+    expect(spyError).toHaveBeenCalledWith(
+      'getCurrentYear() - Invalid fallback value. This value must be a number.',
+    );
+
+    expect(spyWarn).not.toHaveBeenCalled();
+
+    // clear all mock call counts
+    spyError.mockClear();
+    spyWarn.mockClear();
+
+    // TEST 2: fallback is a float
+    const result2 = getCurrentYear(fallback2);
+
+    expect(result2).toBeUndefined();
+
+    // log error when fallback is not a number
+    expect(spyError).toHaveBeenCalledOnce();
+    expect(spyError).toHaveBeenCalledWith(
+      'getCurrentYear() - Invalid fallback value. This value must be a number.',
+    );
+
+    expect(spyWarn).not.toHaveBeenCalled();
+
+    // restore the real system time for other tests
+    vi.useRealTimers();
+  });
+
+  test('getCurrentYear() - returns fallback and logs warning when date function fails', () => {
+    // create mocks for console.error and console.warn to track its calls
+    // and prevent it from actually logging in the test suite
+    const spyError = vi.spyOn(console, 'error').mockImplementation(() => { return; });
+    const spyWarn = vi.spyOn(console, 'warn').mockImplementation(() => { return; });
+
+    // mock the getFullYear() function to throw an error when called
+    const spyDateGetFullYear = vi.spyOn(Date.prototype, 'getFullYear')
+      .mockImplementation(() => {
+        throw new Error('Mocked getFullYear() error');
+      });
+
+    const fallback = 2000;
+
+    const result = getCurrentYear(fallback);
+
+    expect(result).toBe(fallback);
+
+    expect(spyDateGetFullYear).toHaveBeenCalledOnce();
+
+    // no errors logged as fallback is valid
+    expect(spyError).not.toHaveBeenCalled();
+
+    // verify that a console.warn was called with the correct message
+    expect(spyWarn).toHaveBeenCalledOnce();
+    expect(spyWarn).toHaveBeenCalledWith(
+      'getCurrentYear() - Could not compute the current year. Using provided fallback.',
+      expect.objectContaining({ message: 'Mocked getFullYear() error' }),
+    );
+  });
 
 });
 
